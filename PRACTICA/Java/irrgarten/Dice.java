c6703476b2ed8ae4b35f0b7c6bc71c46af19b05e
JorgeBD <baezadiazjorge2112@gmail.com>
2025-09-29 16:39:22 +0200

    Creacion de la clase Dice.java con solo sus atributos de clase

---
+++ PRACTICA/Java/irrgarten/Dice.java
@@ -0,0 +1,21 @@
+package irrgarten;
+
+import java.util.Random;
+
+public class Dice {
+    static private int MAX_USES = 5;
+    static private float MAX_INTELLIGENCE = 10f;
+    static private float MAX_STRENGTH = 10f;
+    static private float RESURRECT_PROB = 0.3f;
+    static private int WEAPON_REWARD = 2;
+    static private int SHIELD_REWARD = 3;
+    static private int HEALTH_REWARD = 5;
+    static private int MAX_ATTACKS = 3;
+    static private int MAX_SHIELD = 2;
+
+    static private Random generator = new Random();
+
+    public int randomPos(int max){
+        return generator.nextInt(max);
+    }
+}
