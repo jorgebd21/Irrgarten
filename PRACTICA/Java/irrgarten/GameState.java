8ba00c7e3c4f56362d57af59b6c5b8e194f0d7c4
JorgeBD <baezadiazjorge2112@gmail.com>
2025-09-29 17:21:40 +0200

    Creacion de la clas GameState.java

---
+++ PRACTICA/Java/irrgarten/GameState.java
@@ -0,0 +1,38 @@
+package irrgarten;
+
+public class GameState {
+    private String laberinth;
+    private String players;
+    private String monsters;
+    private int currentPlayer;
+    private boolean winner;
+    private String log;
+
+    public GameState(String laberinth, String players, String monsters, int currentPlayer, boolean winner, String log) {
+        this.laberinth = laberinth;
+        this.players = players;
+        this.monsters = monsters;
+        this.currentPlayer = currentPlayer;
+        this.winner = winner;
+        this.log = log;
+    }
+
+    public String getLaberinth() {
+        return laberinth;
+    }
+    public String getPlayers() {
+        return players;
+    }
+    public String getMonsters() {
+        return monsters;
+    }
+    public int getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public boolean isWinner() {
+        return winner;
+    }
+    public String getLog() {
+        return log;
+    }
+}
