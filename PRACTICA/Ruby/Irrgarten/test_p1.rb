db2c3fed8a2bf90c7d87ca9985bfa5f8c90906ec
JorgeBD <baezadiazjorge2112@gmail.com>
2025-09-29 18:59:43 +0200

    Arreglo en el archivo test_p1.rb

--- PRACTICA/Ruby/Irrgarten/test_p1.rb
+++ PRACTICA/Ruby/Irrgarten/test_p1.rb
@@ -1,37 +1,35 @@
-class Test_p1
-    def self.main()
-        puts "Prueba P1\nArmas y Escudos"
-        w = Weapon.new(10, 5)
-        puts w.to_s
-        puts w.attack
-        puts w.to_s
-        puts w.discard()
-        s = Shield.new(8, 3)
-        puts s.to_s
-        puts s.protect
-        puts s.to_s
-        puts s.discard()
+puts "Prueba P1\nArmas y Escudos"
 
-        puts "\n Enumerados"
-        puts Orientation::HORIZONTAL
-        puts Game_character::PLAYER
-        puts Directions::DOWN
+w = Weapon.new(10, 5)
+puts w.to_s
+puts w.attack
+puts w.to_s
+puts w.discard
 
-        puts "\nTiradas"
-        for i in 100 do
-            puts Dice.random_pos(10)
-            puts Dice.who_starts(4)
-            puts Dice.random_intelligence()
-            puts Dice.random_strength()
-            puts Dice.resurrect_player()
-            puts Dice.weapon_reward()
-            puts Dice.shield_reward()
-            puts Dice.health_reward()
-            puts Dice.weapon_power()
-            puts Dice.shield_power()
-            puts Dice.uses_left()
-            puts Dice.intensity(5)
-            puts Dice.discard_element(3)
-        end
-    end
+s = Shield.new(8, 3)
+puts s.to_s
+puts s.protect
+puts s.to_s
+puts s.discard
+
+puts "\nEnumerados"
+puts Orientation::HORIZONTAL
+puts Game_character::PLAYER
+puts Directions::DOWN
+
+puts "\nTiradas"
+100.times do
+    puts Dice.random_pos(10)
+    puts Dice.who_starts(4)
+    puts Dice.random_intelligence
+    puts Dice.random_strength
+    puts Dice.resurrect_player
+    puts Dice.weapon_reward
+    puts Dice.shield_reward
+    puts Dice.health_reward
+    puts Dice.weapon_power
+    puts Dice.shield_power
+    puts Dice.uses_left
+    puts Dice.intensity(5)
+    puts Dice.discard_element(3)
 end
