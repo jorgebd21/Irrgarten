bd189c914337dfe155bcea46c79b2dff4b4bf501
JorgeBD <baezadiazjorge2112@gmail.com>
2025-09-29 18:47:04 +0200

    Creacion de la clase GameState.rb

---
+++ PRACTICA/Ruby/Irrgarten/GameState.rb
@@ -0,0 +1,34 @@
+class GameState
+    def initialize(lab, p, m, c, w, l)
+        @labyrinth = lab
+        @players = p
+        @monsters = m
+        @current_player = c
+        @winner = w
+        @log = l
+    end
+
+    def get_labyrinth()
+        return @labyrinth
+    end
+
+    def get_players()   
+        return @players
+    end
+
+    def get_monsters()
+        return @monsters
+    end
+
+    def get_current_player()
+        return @current_player
+    end
+
+    def get_winner()
+        return @winner
+    end
+
+    def get_log()
+        return @log
+    end
+end
