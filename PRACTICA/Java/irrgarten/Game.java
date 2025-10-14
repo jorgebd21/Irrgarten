128914941f4a3aed7d24758971fbe76a159a4503
JorgeBD <baezadiazjorge2112@gmail.com>
2025-10-14 18:10:16 +0200

    Implementacion de los atributos y cabeceras de Game.java

--- PRACTICA/Java/irrgarten/Game.java
+++ PRACTICA/Java/irrgarten/Game.java
@@ -1,5 +1,59 @@
 package irrgarten;
 
 public class Game {
-    
+    static private int MAX_ROUNDS = 10;
+    private int currentPlayerIndex;
+    private String log;
+
+    public Game(int nplayers){
+
+    }
+    public boolean finished(){
+
+    }
+    public boolean nextStep(Directions preferredDirections){
+
+    }
+    public GameState getGameState(){
+
+    }
+    private void configureLabyrinth(){
+
+    }
+    private void nextPlayer(){
+
+    }
+    private Directions actualDirection(Directions preferredDirection){
+
+    }
+    private GameState combat(Monster monster){
+
+    }
+    private void manageReward(GameState winner){
+
+    }
+    private void manageResurrection(){
+
+    }
+    private void logPlayerWon(){
+
+    }
+    private void logMonsterWon(){
+
+    }
+    private void Resurrected(){
+
+    }
+    private void logPlayerSkipTurn(){
+
+    }
+    private void logPlayerNoOrders(){
+
+    }
+    private void logNoMonster(){
+
+    }
+    private void logRound(int round, int max){
+
+    }
 }
