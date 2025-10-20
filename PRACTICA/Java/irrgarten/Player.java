bc18c9933d7f77d50bac033f9814c46b7ddaee99
JorgeBD <baezadiazjorge2112@gmail.com>
2025-10-20 15:52:43 +0200

    Implementacion completa de Player.java

--- PRACTICA/Java/irrgarten/Player.java
+++ PRACTICA/Java/irrgarten/Player.java
@@ -19,11 +19,21 @@ public class Player {
     private Shield[] shields;
 
     public Player(char number, float intelligence, float strength) {
-
+        this.name = "Player #" + number;
+        this.number = number;
+        this.intelligence = intelligence;
+        this.strength = strength;
+        this.health = INITIAL_HEALTH;
+        this.consecutiveHits = 0;
+        this.weapons = new Weapon[MAX_WEAPONS];
+        this.shields = new Shield[MAX_SHIELDS];
     }
 
     public void resurrect() {
-
+        weapons = new Weapon[MAX_WEAPONS];
+        shields = new Shield[MAX_SHIELDS];
+        health = INITIAL_HEALTH;
+        consecutiveHits = 0;
     }
 
     public int getRow() {
@@ -36,16 +46,21 @@ public class Player {
         return number;
     }
     public void setPos(int row, int col) {
-
+        this.row = row;
+        this.col = col;
     }
     public boolean dead() {
-        throw new UnsupportedOperationException();
+        return health<0;
     }
     public Directions move(Directions direction, Directions[] validMoves){
         throw new UnsupportedOperationException();
     }
     public float attack(){
-        throw new UnsupportedOperationException();
+        float sum = strength;
+        for(int i=0; i<MAX_WEAPONS ; i++){
+            sum = weapons[i].attack();
+        }
+        return sum;
     }
     public boolean defend(float receivedAttack){
         throw new UnsupportedOperationException();
@@ -54,7 +69,7 @@ public class Player {
 
     }
     public String toString(){
-        throw new UnsupportedOperationException();
+        return name + " (HP: " + health + ", Pos: [" + row + "," + col + "], INT:" + intelligence + ", STR:" + strength + ")";
     }
 
     private void receiveWeapon(Weapon w){
@@ -64,30 +79,38 @@ public class Player {
 
     }
     private Weapon newWeapon(){
-        throw new UnsupportedOperationException();
+        return new Weapon(Dice.weaponPower(), Dice.usesLeft());
     }
     private Shield newShield(){
-        throw new UnsupportedOperationException();
+        return new Shield(Dice.shieldPower(), Dice.usesLeft());
     }
     private float sumWeapons(){
-        throw new UnsupportedOperationException();
+        float sum = 0;
+        for(int i=0; i<MAX_WEAPONS ; i++){
+            sum += weapons[i].attack();
+        }
+        return sum;
     }
     private float sumShields(){
-        throw new UnsupportedOperationException();
+        float sum = 0;
+        for(int i=0; i<MAX_SHIELDS ; i++){
+            sum += shields[i].protect();
+        }
+        return sum;
     }
     private float defensiveEnergy(){
-        throw new UnsupportedOperationException();
+        return sumShields() + intelligence;
     }
     private boolean manageHit(float receivedAttack){
         throw new UnsupportedOperationException();
     }
     private void resetHits(){
-        
+        consecutiveHits = 0;
     }
     private void gotWounded(){
-        
+        health--;
     }
     private void incConsecutiveHits(){
-        
+        consecutiveHits++;
     }
 }
